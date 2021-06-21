f0156263c6d421ea393dda67ab5397827ad7a5a2
Jorge Barrientos AKA LagunaGFX <lagunagfx@lagunalabs.eu>
2021-06-21 19:05:11 +0200

    Following the getopt() short arg implementation

--- src/charset-extract.c
+++ src/charset-extract.c
@@ -2,12 +2,12 @@
 
     Description:
     - 8 bit Character ROM visualizer for the original ZX Spectrum
- 
-    Features:
-    - Extract the charset to a single binary file 
 
     Author:
     - Jorge Barrientos Poisson
+ 
+    Features:
+    - Extract the charset to a single binary file 
 
     Requirements:
 
@@ -16,6 +16,7 @@
     stdlib	exit() function
     stbbool	boolean data type
     unistd	getopt() function
+    ctype	isprint() function
 
     ChangeLog:
     
@@ -29,6 +30,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <ctype.h>
 
 #define CHARSET_OFFSET	15616	// position of the CHARSET within the ZX Spectrum ROM
 #define CHARSET_SIZE	96*8	// 96 characters, 8 bytes each 
@@ -38,9 +40,9 @@ int count = CHARSET_SIZE;
 char character;
 
 void usage() {
-    printf("Usage: charset-extractor -f <infile> <outfile>\n");
-    printf("   -t,   --text          Outputs a plain ASCII text representation of the charset to the standard output\n");
-    printf("   -r,   --raw           Extracts the raw binary charset from the ROM file, without modification");
+    printf("Usage: charset-extractor [options] [-i infile] <outfile>\n");
+    printf("   -a,   --ascii         Outputs a plain ASCII text representation of the charset to the standard output\n");
+    printf("   -r,   --raw           Extracts the raw binary charset from the ROM file, without modification\n");
     printf("   -h,   --help          Show this help message\n");
 }
 
@@ -58,79 +60,88 @@ void drawbytes(char b) {
 
 int main( int argc, char **argv ) {
 
+    char * filename;
     int cli;
-    bool outAscii = false;
-    bool outBinaryFile = false;
+    bool out_ascii = false;
+    bool out_raw = false;
 
-    while ( ( cli = getopt(argc,argv,"frth?") ) != -1 ) {
-	switch(cli) {
-	    case 'f':
-		//if ( argv[] )
-		//romfile = fopen
-		break;
-	    case 't':
-		outAscii = true;
-		break;
-	    case 'r':
-		outBinaryFile = true;
-		break;
-	    case 'h':
-		usage();
-		exit(0);
-	    case '?':
-		usage();
-		exit(0);
-	    default:
-		usage();
-		exit(0);	
-	} 
-    }
-
-    if ( argc >= 2 ) {
-
-	//romfile = fopen(argv[1],"rb");
-	romfile = fopen("48.rom","rb");
-
-	if ( outBinaryFile )
-	    charset = fopen(argv[2],"wb");
-
-	if ( romfile == NULL ) {
-	    printf("Error opening file\n");
-	    return -1;
-	} else {
-	    printf("ROM file: %s\n", argv[1]);
+    if ( argc < 2 ) {
+	usage();
+	exit(0);
+    } else {
+	while ( ( cli = getopt(argc,argv,"ahi:r") ) != -1 ) {
+	    switch(cli) {
+		case 'i':
+		    printf("--> ROM file : %s\n", optarg);
+		    filename = optarg;
+		    break;
+		case 'a':
+		    out_ascii = true;
+		    break;
+		case 'r':
+		    out_raw = true;
+		    break;
+		case 'h':
+		    usage();
+		    exit(0);
+		case '?':
+		    switch(optopt) {
+			case 'i':
+			    printf("ERROR: A valid ROM file path must be provided with the -%c function\n",optopt);
+			    break;
+			default:
+			    if ( isprint(optopt) ) 
+				printf("ERROR: Option -%c not recognized\n",optopt);
+			    else
+				printf("ERROR: Option contains non-printable characters\n");
+			    break;
+		    }
+		    usage();
+		    exit(-1);	
+	    } 
 	}
-
-	printf("Offset %i",CHARSET_OFFSET) ;
-	fseek(romfile, CHARSET_OFFSET, SEEK_CUR);
-
-	while ( count > 0 ) {
-
-	    if ( count % 8 == 0 )
-	       printf("\n");	
-
-	    printf("Byte: %3i \t",count);
-
-	    character = fgetc(romfile);
-
-	    if ( outBinaryFile )
-		fputc(character,charset);
-
-	    if ( outAscii)
-		drawbytes(character);
-	    
-	    count--;
-	    //if ( count == 0 )
-	    //	break;
-	}	
-
-	printf("\n");
-
-	// close the ROM input file
-	fclose(romfile);
-	// close the CHARSET output file
-	fclose(charset);
     }
+
+    printf("Opening ROM file %s\n", filename);
+    romfile = fopen(filename,"rb");
+
+    if ( out_raw )
+	charset = fopen(argv[2],"wb");
+
+    if ( romfile == NULL ) {
+	printf("Error opening file\n");
+	return -1;
+    } else {
+	printf("ROM file: %s\n", filename);
+    }
+
+    printf("Offset %i",CHARSET_OFFSET) ;
+    fseek(romfile, CHARSET_OFFSET, SEEK_CUR);
+
+    while ( count > 0 ) {
+
+	if ( count % 8 == 0 )
+	   printf("\n");	
+
+	printf("%3i ",count);
+
+	character = fgetc(romfile);
+
+	if ( out_raw )
+	    fputc(character,charset);
+
+	if ( out_ascii)
+	    drawbytes(character);
+	
+	count--;
+    }	
+
+    printf("\n");
+
+    // close the ROM input file
+    fclose(romfile);
+    // close the CHARSET output file
+    fclose(charset);
 
     return 0;
 }
